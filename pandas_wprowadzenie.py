fe2ef924ccdf442a23cbe99d03a712ffb0821d57
OliwierWilbrandt <161207691+OliwierWilbrandt@users.noreply.github.com>
2024-06-05 17:11:36 +0200

    dodanie pandas

---
+++ pandas_wprowadzenie.py
@@ -0,0 +1,69 @@
+#Zad 1
+
+import pandas as pd
+
+# plik = pd.ExcelFile('imiona.xlsx')
+# df = pd.read_excel(plik, header=0)
+
+#Zad2
+
+# print(df[df.Liczba > 1000])
+# print('')
+# print(df[df.Imie == 'RADOSŁAW'])
+# print('')
+# print(df.Liczba.sum())
+# print('')
+# grupa = df[df.Rok < 2006]
+# print(grupa.Liczba.sum())
+# grupa = df[df.Rok < 2006].groupby('Rok').agg({'Liczba': ['sum']})
+# print(grupa)
+# print('')
+#
+# print(df.groupby('Plec').agg({'Liczba':['sum']}))
+#
+# print(df.sort_values('Liczba', ascending=False).groupby(['Rok', 'Plec']).first())
+#
+# new_df = df.sort_values('Liczba', ascending=False).groupby(['Rok', 'Plec'])
+#
+# for index, group in enumerate(new_df, start=1):
+#     print(f"{index} {group[0]}")
+#     print(f" {group[1].iloc[0]['Imie']}", end='')
+#     print(f" {group[1].iloc[0]['Liczba']}")
+# grupa = df.groupby(['Rok', 'Plec']).agg('Liczba').max()
+# print(grupa)
+#
+# for i,g in enumerate(grupa, start=1):
+#     print(i)
+#     print(df[df['Liczba'] == g])
+# grupa = df.groupby(['Rok', 'Plec']).agg({'Liczba':['max']})
+# print(grupa[max])
+# for i, g in enumerate(grupa[max], start=1):
+#     print(df[df['Liczba'] == g])
+# print('')
+# print("Chłopiec")
+# print(df[df['Plec'] == 'M'].groupby(['Imie']).agg({'Liczba': ['sum']}).sort_values(('Liczba', 'sum'),
+#                                                                                    ascending=False).iloc[0])
+# print("Dziewczynka")
+# print(df[df['Plec'] == 'K'].groupby(['Imie']).agg({'Liczba': ['sum']}).sort_values(('Liczba', 'sum'),
+#                                                                                    ascending=False).iloc[0])
+
+#Zad 3
+
+# df = pd.read_csv('zamowienia.csv', header=0, sep=';', decimal='.')
+#
+# print(df['Sprzedawca'].unique())
+# # print('')
+# print(df.sort_values('Utarg', ascending=False).head(5))
+# # print('')
+# print(df.groupby('Sprzedawca').size())
+# print('')
+# print(df.groupby('Kraj').agg({'Utarg': ['sum']}))
+# print(df.groupby('Kraj').size())
+# # print('')
+# print(df[(df['Kraj'] == 'Polska') & (df['Data zamowienia'] >= '2005-01-01') & (df['Data zamowienia'] <= '2005-12-31')].
+#       agg({'Utarg': ['sum']}))
+# # print('')
+# print(df[df['Data zamowienia'].str[:4] == '2004'].agg({'Utarg': ['mean']}))
+# rok_2004 = df[((df[ 'Data zamowienia'] >= '2004-01-01') & (df['Data zamowienia'] <= '2004-12-31'))]
+# rok_2004.to_csv("zamowienia_2004.csv", index=False)
+#
